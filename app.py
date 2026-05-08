4028961185d6bdad439000cea66e3b5dc13efb18
krushna-g <kgade3253@gmail.com>
2026-05-08 16:14:08 +0530

    Create database tables on Render startup

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from S2Digital import create_app
+from S2Digital.extensions import db
 import os
 
 config = os.environ.get("S2Digital_CONFIG")
 app = create_app(config)
+
+with app.app_context():
+    db.create_all()
